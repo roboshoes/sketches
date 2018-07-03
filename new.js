ea0faab79dc83d2a747c899f6a5b935090accc78
Mathias Paumgarten <mail@mathias-paumgarten.com>
2018-07-03 14:34:09 -0700

    adds script to generate TS sketch

---
+++ new.js
@@ -0,0 +1,22 @@
+const fs = require( "fs" );
+const path = require( "path" );
+const exec = require( "child_process" ).exec;
+const nextHighest = fs
+    .readdirSync( ".", "utf8" )
+    .filter( name => !isNaN( Number( name ) ) )
+    .reduce( ( highest, current ) => Math.max( highest, current ), 0 ) + 1;
+
+const name = process.argv[ 2 ] || nextHighest.toString();
+
+if ( ! fs.existsSync( name ) ) {
+
+    console.log( "-> Generating source" );
+    exec( `cp -r _template/ ${ name }/`, () => {
+
+        console.log( "-> Installing packages" );
+        exec( "npm install --save-dev parcel-bundler typescript", { cwd: name }, () => {
+
+            console.log( `\nFolder: ${ name }` );
+        } );
+    } );
+}

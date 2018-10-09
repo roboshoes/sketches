b30f9a20fe967a68377fd2cc4f9a06465e31d3b8
Mathias Paumgarten <mail@mathias-paumgarten.com>
2018-10-08 21:43:30 -0700

    throbby loopy

--- 16/main.ts
+++ 16/main.ts
@@ -12,32 +12,37 @@ const DIVIDER = 400;
 options( {
     size: [ 1024, 1024 ],
     record: false,
-    frames: 1,
-    color: "#741974",
+    frames: 120,
+    color: "black",
+    clear: true,
 } );
 
-draw( ( context: CanvasRenderingContext2D, ) => {
-
+draw( ( context: CanvasRenderingContext2D, _, t: number ) => {
     for ( let y = 0; y < SIZE; y++ ) {
         for ( let x = 0; x < SIZE; x++ ) {
+
             const centerX = TILE * x + TILE / 2;
             const centerY = TILE * y + TILE / 2;
 
             const pointX: number = noiseX.simplex2( centerX / DIVIDER, centerY / DIVIDER ) * 0.5 + 0.5;
             const pointY: number = noiseY.simplex2( centerX / DIVIDER, centerY / DIVIDER ) * 0.5 + 1;
 
+            const distance = Math.sqrt( centerX * centerX + centerY * centerY );
+            const distancePercent = distance / ( Math.sqrt( 2 ) * 1024 );
+
+            const offset = Math.sin( ( ( t + distancePercent ) % 1 ) * Math.PI ) ;
 
             context.beginPath();
             context.moveTo( centerX + RADIUS, centerY );
             context.arc( centerX, centerY, RADIUS, 0, Math.PI * 2 );
-            context.lineWidth = 0 + pointX * 6.5;
+            context.lineWidth = pointX * 6.5 * offset;
             context.strokeStyle = "white";
             context.stroke();
 
             context.save();
             context.beginPath();
             context.translate( centerX, centerY );
-            context.rotate( pointY * Math.PI * 2 );
+            context.rotate( pointY + ( Math.PI * 2 * t ) );
             context.moveTo( 0, 0 );
             context.lineTo( RADIUS - 4, 0 );
             context.arc( 0, 0, RADIUS - 4, 0, Math.PI * 2 * pointY );
